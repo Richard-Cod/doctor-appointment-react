748ff62b7d60f50b3b1b6deb77c6f5f050181703
Richard-Cod <richard.bathiebo.7@gmail.com>
2022-01-07 19:41:51 +0100

    e

--- src/Pages/ForgetPasswordPage/index.js
+++ src/Pages/ForgetPasswordPage/index.js
@@ -1,25 +1,61 @@
-import routes from '../../constants/routes'
-import AuthLayout from '../../templates/layout/AuthLayout'
+import { useFormik } from 'formik';
+import { useNavigate, useParams } from 'react-router-dom';
+import Input from '../../components/shared/Input';
+import routes from '../../constants/routes';
+import ForgotPasswordConfirmPageVM from '../../logic/ForgotPasswordConfirmPageVM';
+import AuthLayout from '../../templates/layout/AuthLayout';
+import ProfilLayout from '../../templates/layout/ProfilLayout';
 
-function ForgotPasswordPage() {
+const forgotPasswordConfirmPageVM = new  ForgotPasswordConfirmPageVM()
+function Form() {
+    const { uid , token } = useParams();
+   const navigate =  useNavigate()
+
+    const formik = useFormik({
+        initialValues: forgotPasswordConfirmPageVM.initialValues,
+        validationSchema: forgotPasswordConfirmPageVM.formSchema,
+        onSubmit: values => {
+          try {
+            forgotPasswordConfirmPageVM.submitForm(values.email)
+          } catch (error) {
+            
+          }
+        },
+      });
+
+      
+   
+      return (
+        <form onSubmit={formik.handleSubmit}>
+           <div className="login-header">
+            <h3>Forgot Password?</h3>
+            <p className="small text-muted">Enter your email to get a password reset link</p>
+          </div>
+
+            <Input label="Email" name="email" type="text" formik={formik} />
+            <div className="text-right">
+              <a className="forgot-link" href={routes.login}>Remember your password?</a>
+            </div>
+            <button 
+              disabled={formik.isSubmitting}
+              className="btn btn-primary btn-block btn-lg login-btn" 
+              type="submit">
+                Reset Password
+            </button>
+        </form>
+      )  
+}
+
+
+
+
+function ForgotPasswordConfirmPage() {
     return (
         <AuthLayout>
-                  <div className="login-header">
-                    <h3>Forgot Password?</h3>
-                    <p className="small text-muted">Enter your email to get a password reset link</p>
-                  </div>
-                  <form action="">
-                    <div className="form-group form-focus">
-                      <input type="email" className="form-control floating" />
-                      <label className="focus-label">Email</label>
-                    </div>
-                    <div className="text-right">
-                      <a className="forgot-link" href={routes.login}>Remember your password?</a>
-                    </div>
-                    <button className="btn btn-primary btn-block btn-lg login-btn" type="submit">Reset Password</button>
-                  </form>
-        </AuthLayout>
+         
+        <Form />
+     </AuthLayout>
     )
 }
 
-export default ForgotPasswordPage
+export default ForgotPasswordConfirmPage
